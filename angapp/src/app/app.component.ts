0386bd95e3fe717d9bcf3c953488011a20ecda41
charan123keerthipati <charan123keerthipati@gmail.com>
2021-07-19 16:50:03 +0530

    Employee Details

---
+++ angapp/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app';
+  myimage:String="assets/images/kp.png";
+}
