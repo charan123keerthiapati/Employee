b9b00f44c7253179b596a2f20c9dc0eb2fbfa09a
charan123keerthipati <charan123keerthipati@gmail.com>
2021-07-20 21:02:03 +0530

    Employee Details

--- angapp/src/app/login/login.component.ts
+++ angapp/src/app/login/login.component.ts
@@ -6,11 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  
 
   constructor() { }
 
   ngOnInit() {
-    
+
+  }
+  onSubmit(user){
+    console.log(user);
   }
 
 }
